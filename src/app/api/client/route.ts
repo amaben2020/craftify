b419b70fe421bd31a2ffe1365865387e27d3eed9
algomachine007 <amaben202phantom@gmail.com>
2024-04-26 10:28:49 +0100

    creating create client profile

--- src/app/api/client/route.ts
+++ src/app/api/client/route.ts
@@ -1,10 +1,33 @@
+import prisma from '@lib/prisma';
 import { NextRequest, NextResponse } from 'next/server';
 
-export const POST = (request: NextRequest) => {
+export const POST = async (request: NextRequest) => {
   try {
     console.log('Called');
 
-    return NextResponse.json('Called');
+    const data = await prisma.client.create({
+      data: {
+        profile: {
+          create: {
+            email: 'amaben1@gmail.com',
+            firstName: 'Amala',
+            lastName: 'Uzor',
+            image: '',
+            address: 'Dugbana',
+          },
+        },
+      },
+    });
+
+    return NextResponse.json(
+      {
+        data,
+      },
+      {
+        status: 201,
+        statusText: 'Created',
+      }
+    );
   } catch (error) {
     console.log(error);
   }
