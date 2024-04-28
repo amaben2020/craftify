f97f39d922b0921e1ac0bddc86b06c27546fbdc5
algomachine007 <amaben202phantom@gmail.com>
2024-04-28 16:26:42 +0100

    connectin app to prisma instance

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,13 @@
+import { auth } from '@/auth/auth';
 import { SignIn } from '@/components/sign-in/sign-in';
+import Image from 'next/image';
 
-export default function Home() {
+export default async function Home() {
+  const user = await auth();
+  console.log(user?.user?.image);
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <Image src={user?.user?.image ?? ''} alt="" width={600} height={400} />
       <SignIn />
     </main>
   );
--- src/auth/auth.ts
+++ src/auth/auth.ts
@@ -1,13 +1,12 @@
 import { PrismaAdapter } from '@auth/prisma-adapter';
-import { PrismaClient } from '@prisma/client';
+import prisma from '@lib/prisma';
 import NextAuth from 'next-auth';
+import { Adapter } from 'next-auth/adapters';
 import github from 'next-auth/providers/github';
 import google from 'next-auth/providers/google';
 
-const prisma = new PrismaClient();
-
 export const { handlers, auth, signIn, signOut } = NextAuth({
   trustHost: true,
-  adapter: PrismaAdapter(prisma),
+  adapter: PrismaAdapter(prisma) as Adapter,
   providers: [google, github],
 });
